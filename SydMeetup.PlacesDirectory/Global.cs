57e90576e12176e07782e634ecf25f24777877f7
Aria Zanganeh <admin@simpleidea.com.au>
2018-06-16 14:01:39 +1000

    Initial commit - Episerver basic structure

---
+++ SydMeetup.PlacesDirectory/Global.cs
@@ -0,0 +1,8 @@
+﻿namespace SydMeetup.PlacesDirectory
+{
+    public class Global
+    {
+        public static readonly string LoginPath = "/util/login.aspx";
+        public static readonly string AppRelativeLoginPath = string.Format("~{0}", LoginPath);
+    }
+}
